f3abf7f012a626eaeb55e582b7071c8a861122ee
Gustbel <gustavobelbruno@gmail.com>
2023-08-29 14:57:50 -0300

    deleted old deployment and added new pools of Pools

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ const tangleseaPairAbi = require('./abi/tangleseaPair.json');
 
 const wsmrPrice = 0.05
 const usdtPrice = 1.02
+
 // POOLS
 const vaultAddress = "0x8021c957a0FF43ee4aF585D10a14bD14C30b89F7"
-const pool1Id = "0xd04978fce521e644ebddaa1aabcad6d36b518d34000200000000000000000005"
-const pool2Id = '0xcd0840650045bf8773e290b67ba6169e28c7b703000000000000000000000007'
+const pool1Id = "0x4bfd8353eddf067588d3a48389fcdbe3777f9ba4000200000000000000000008"
+const pool2Id = '0xc800de05df867e81fcb3fddd16baf0ce4db64b70000000000000000000000009'
 
 // SHIMMERSEA
-const shimmerSeaPool1Address = "0x4e924F6a6AC5452D6E1cB08818Fb103Fd0328eb0"
-const shimmerSeaPool2Address = "0xA27BEcBC6098363cB9784f41F001D74604fa75b1"
+//const shimmerSeaPool1Address = "0x4e924F6a6AC5452D6E1cB08818Fb103Fd0328eb0"
+//const shimmerSeaPool2Address = "0xA27BEcBC6098363cB9784f41F001D74604fa75b1"
 
 // Object definition
 function Pool(price, collatTokenBalance, vusdBalance, capitalization, dominance) {
@@ -64,6 +65,7 @@ function App() {
       const pool2PoolsVusdPrice = poolCotization * usdtPrice
       const pool2PoolsCap = pool2PoolsUsdtBalance * usdtPrice + pool2PoolsVusdBalance * pool2PoolsVusdPrice
 
+      /*
       // Get price from ShimmerSea
       // Pool1
       const shimmerSeaPool1 = new ethers.Contract(shimmerSeaPool1Address, tangleseaPairAbi, provider);
@@ -81,9 +83,18 @@ function App() {
       poolCotization = pool2SseaUsdtBalance/pool2SseaVusdBalance
       const pool2SseaVusdPrice = poolCotization * usdtPrice
       const pool2SseaCap = pool2SseaUsdtBalance * usdtPrice + pool2SseaVusdBalance * pool2SseaVusdPrice
-      
+      */
 
       // Dominance calculation
+      // temporal
+      const pool1SseaCap = 0
+      const pool2SseaCap = 0
+      const pool1SseaVusdPrice = 0
+      const pool2SseaVusdPrice = 0
+      const pool1SseaVusdBalance = 0
+      const pool2SseaVusdBalance = 0
+      /////////
+
       const marketCap = pool1PoolsCap + pool2PoolsCap + pool1SseaCap + pool2SseaCap
       const pool1PoolsDominance = pool1PoolsCap / marketCap
       const pool2PoolsDominance = pool2PoolsCap / marketCap
@@ -118,18 +129,18 @@ function App() {
         (pool2PoolsDominance * 100).toFixed(2)
       )
       addPool(
-        pool1SseaVusdPrice.toFixed(5), 
-        pool1SseaWsmrBalance.toFixed(0), 
-        pool1SseaVusdBalance.toFixed(0), 
-        pool1SseaCap.toFixed(0), 
-        (pool1SseaDominance * 100).toFixed(2)
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
       )
       addPool(
-        pool2SseaVusdPrice.toFixed(5), 
-        pool2SseaUsdtBalance.toFixed(0), 
-        pool2SseaVusdBalance.toFixed(0), 
-        pool2SseaCap.toFixed(0), 
-        (pool2SseaDominance * 100).toFixed(2)
+        "...",
+        "...",
+        "...",
+        "...",
+        "...",
       )
       setPools(poolArray)
 
